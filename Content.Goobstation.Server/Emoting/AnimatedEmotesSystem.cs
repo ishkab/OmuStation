73188c2449eb1cb4bac33cc08d1030b9f7b98e63
Androidonator <33227516+Androidonator@users.noreply.github.com>
2026-09-03 09:26:28 +0200

    added cvar for flipping dodge (#7106)
    * cvars
    
    * summary

--- Content.Goobstation.Server/Emoting/AnimatedEmotesSystem.cs
+++ Content.Goobstation.Server/Emoting/AnimatedEmotesSystem.cs
@@ -3,17 +3,24 @@
 using Content.Goobstation.Shared.Emoting;
 using Content.Shared.Chat;
 using Content.Shared.Chat.Prototypes;
+using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
+using Content.Goobstation.Common.CCVar;
+using Content.Shared.CCVar;
 
 namespace Content.Goobstation.Server.Emoting;
 
 public sealed partial class AnimatedEmotesSystem : SharedAnimatedEmotesSystem
 {
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    private bool _flipDodgeEnabled = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<AnimatedEmotesComponent, EmoteEvent>(OnEmote);
+        Subs.CVar(_cfg, GoobCVars.FlipDodgeEnabled, value => _flipDodgeEnabled = value, true);
     }
 
     private void OnEmote(Entity<AnimatedEmotesComponent> ent, ref EmoteEvent args)
@@ -26,7 +33,7 @@ public sealed partial class AnimatedEmotesSystem : SharedAnimatedEmotesSystem
         ent.Comp.Emote = prot;
         Dirty(ent);
 
-        /*if (prot == "Flip") Omu kill
+        /*if ((prot == "Flip") && _flipDodgeEnabled) Omu kill
             ApplyFlipEffects(ent);*/
     }
 }
